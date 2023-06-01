b7900a4766680145677b183e9e7508cc19a9a94a
yankzsoe <yankzsoe@gmail.com>
2023-06-01 10:46:47 +0700

    fixing dsn

--- app/configs/Database.go
+++ app/configs/Database.go
@@ -25,13 +25,8 @@ func InitDB() {
 	onceDb.Do(func() {
 		// Create connection to postgres
 		dbhost := os.Getenv("dblocalhost")
-		dbname := os.Getenv("dbname")
-		dbpassword := os.Getenv("dbpassword")
-		dbusername := os.Getenv("dbusername")
-		dbport := os.Getenv("dbport")
 
-		dsn := "host=" + dbhost + " user=" + dbusername + " password=" + dbpassword + "dbname=" + dbname + " port=" + dbport + " sslmode=disable TimeZone=Asia/Jakarta"
-		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		db, err := gorm.Open(postgres.Open(dbhost), &gorm.Config{
 			Logger: customLogger,
 			NamingStrategy: schema.NamingStrategy{
 				SingularTable: true,
