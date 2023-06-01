bf9d291f6204ea7e8cc7dca6464addc4f2e5625e
yankzsoe <yankzsoe@gmail.com>
2023-06-01 11:43:11 +0700

    update swagger host

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	config "golang_api/app/configs"
 	"golang_api/app/routers"
+	"os"
 
 	"golang_api/app/migrations"
 	"golang_api/docs"
@@ -37,7 +38,7 @@ func main() {
 	docs.SwaggerInfo.Title = "Swagger User API"
 	docs.SwaggerInfo.Description = "This is a sample Swagger in Golang with GIN Framework."
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "127.0.0.1:5001/api"
+	docs.SwaggerInfo.Host = os.Getenv("hostSwagger")
 	docs.SwaggerInfo.BasePath = "/v1"
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
