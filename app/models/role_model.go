41328ec3aa2750ff086fae0eda0cc2ee13080caa
yankzsoe <yankzsoe@gmail.com>
2023-06-10 17:08:44 +0700

    fix on find all users

--- app/models/role_model.go
+++ app/models/role_model.go
@@ -5,7 +5,7 @@ import "time"
 type RoleModel struct {
 	ID          string     `gorm:"column:role_id;primaryKey;type:uuid;default:gen_random_uuid()"`
 	Name        string     `gorm:"column:role_name;not null;size:100"`
-	IsActive    bool       `gorm:"column:is_active;not null;default:"`
+	IsActive    bool       `gorm:"column:is_active;not null;default:true"`
 	CreatedDate time.Time  `gorm:"column:created_date;autoCreateTime:true;"`
 	UpdatedDate *time.Time `gorm:"column:updated_date;autoUpdateTime:false"`
 }
