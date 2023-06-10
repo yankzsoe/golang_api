6fbbe20223e6e3dc0429e63112b1a3f3ee15d56c
yankzsoe <yankzsoe@gmail.com>
2023-06-10 23:10:11 +0700

    make index & unique for role's name & module's name

--- app/models/module_model.go
+++ app/models/module_model.go
@@ -4,7 +4,7 @@ import "time"
 
 type ModuleModel struct {
 	ID          string     `gorm:"column:module_id;primaryKey;type:uuid;default:gen_random_uuid()"`
-	Name        string     `gorm:"column:module_name;not null;size:100"`
+	Name        string     `gorm:"column:module_name;not null;index;unique;size:100"`
 	Remark      string     `gotm:"column:remark;size:250"`
 	CreatedDate time.Time  `gorm:"column:created_date;autoCreateTime:true;"`
 	UpdatedDate *time.Time `gorm:"column:updated_date;autoUpdateTime:false"`
