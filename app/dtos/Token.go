ce868ec2389bea247cc31c5fa9d302d66f0265af
yankzsoe <yankzsoe@gmail.com>
2023-06-18 20:45:25 +0700

    rename body from `value` to `token`

--- app/dtos/Token.go
+++ app/dtos/Token.go
@@ -3,14 +3,14 @@ package dtos
 import "time"
 
 type Token struct {
-	Value        string       `json:"value"`
+	Value        string       `json:"token"`
 	IssuedOn     time.Time    `json:"issuedOn"`
 	ExpiresOn    time.Time    `json:"expiresOn"`
 	RefreshToken RefreshToken `json:"refreshToken"`
 }
 
 type RefreshToken struct {
-	Value     string    `json:"value"`
+	Value     string    `json:"token"`
 	IssuedOn  time.Time `json:"issuedOn"`
 	NotBefore time.Time `json:"notBefore"`
 	ExpiresOn time.Time `json:"expiresOn"`
